763729d0c615d8d244695780bac5bdeb0f3f2c3d
Benjamin Franzke <benjaminfranzke@googlemail.com>
2016-02-01 06:18:08 +0100

    Stop using extended regexp's
    There's no need to use them

--- add_extbase_property.sh
+++ add_extbase_property.sh
@@ -58,8 +58,8 @@ tca_file=Configuration/TCA/${tablename}.php
 
 [[ -e "$tca_file" ]] || tca_file=Configuration/TCA/${model}.php
 
-field=`echo $property | sed -r ':a;s/([a-z]+)([A-Z][a-z]+)/\1_\l\2/g;ta'`
-uproperty=`echo $property | sed -r 's/^./\u&/'`
+field=`echo $property | sed ':a;s/\([a-z]\)\([A-Z][a-z]\)/\1_\l\2/g;ta'`
+uproperty=`echo $property | sed 's/^./\u&/'`
 
 
 declare -A type_map=(
